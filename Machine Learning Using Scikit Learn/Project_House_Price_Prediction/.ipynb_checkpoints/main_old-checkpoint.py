8547624ea10606c30d4c775e6f8e317babce6ffa
razesoni <akash27590@gmail.com>
2026-07-09 20:27:43 +0530

    09-07-2026

---
+++ Machine Learning Using Scikit Learn/Project_House_Price_Prediction/.ipynb_checkpoints/main_old-checkpoint.py
@@ -0,0 +1,88 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import root_mean_squared_error
+from sklearn.model_selection import cross_val_score
+
+# 1. load the  dataset
+housing = pd.read_csv("housing.csv")
+
+# 2. Create a stratified test set
+housing['income_cat'] = pd.cut(housing["median_income"],
+                               bins = [0.0, 1.5, 3.0, 4.5, 6.0, np.inf],
+                               labels= [1, 2, 3, 4, 5])
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.iloc[train_index].drop("income_cat", axis=1)
+    strat_test_set = housing.iloc[test_index].drop("income_cat", axis=1)
+
+# We will work on the copy of training data
+housing = strat_train_set.copy()
+
+# 3. Separate features and labels
+housing_label = housing["median_house_value"].copy()
+housing = housing.drop("median_house_value", axis=1)
+
+# 4. List numerical and categorical columns
+num_attribs = housing.drop("ocean_proximity", axis=1).columns.tolist()
+cat_attribs = ["ocean_proximity"]
+
+# 5. Let's make me pipeline 
+
+# For numerical columns
+num_pipeline = Pipeline([
+    ("imputer", SimpleImputer(strategy='median')),
+    ("scaler", StandardScaler())
+])
+
+# For categorical columns
+cat_pipeline = Pipeline([
+    ("onehot", OneHotEncoder(handle_unknown="ignore"))
+])
+
+# Construct the full pipeline
+full_pipeline = ColumnTransformer([
+    ("num", num_pipeline, num_attribs),
+    ("cat", cat_pipeline, cat_attribs)
+])
+
+# 6. Transform the data
+housing_prepared = full_pipeline.fit_transform(housing)
+
+# 7. Train the model
+
+# linear Regression Model
+lin_reg = LinearRegression()
+lin_reg.fit(housing_prepared, housing_label)
+lin_preds = lin_reg.predict(housing_prepared)
+# lin_rmse = root_mean_squared_error(housing_label, lin_preds)
+lin_rmses = -cross_val_score(lin_reg, housing_prepared, housing_label, scoring="neg_root_mean_squared_error", cv=10)
+# print(f"The root mean squared error for Linear Regression is {lin_rmse}")
+print(pd.Series(lin_rmses).describe())
+
+# Decision Tree Model
+dec_reg = DecisionTreeRegressor()
+dec_reg.fit(housing_prepared, housing_label)
+dec_preds = dec_reg.predict(housing_prepared)
+# dec_rmse = root_mean_squared_error(housing_label, dec_preds)
+dec_rmses = -cross_val_score(dec_reg, housing_prepared, housing_label, scoring="neg_root_mean_squared_error", cv=10)
+# print(f"The root mean squared error for Linear Regression is {dec_rmses}")
+print(pd.Series(dec_rmses).describe())
+
+# Random Forest Regressor Model
+ran_reg = RandomForestRegressor()
+ran_reg.fit(housing_prepared, housing_label)
+ran_preds = ran_reg.predict(housing_prepared)
+# ran_rmse = root_mean_squared_error(housing_label, ran_preds)
+ran_rmses = -cross_val_score(ran_reg, housing_prepared, housing_label, scoring="neg_root_mean_squared_error", cv=10)
+# print(f"The root mean squared error for Linear Regression is {ran_rmse}")
+print(pd.Series(ran_rmses).describe())
